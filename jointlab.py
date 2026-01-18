6615d342d46f788bfbfb20ba604c449a38a3b1f4
ianrUTEP <79346735+ianrUTEP@users.noreply.github.com>
2026-01-18 00:34:55 -0700

    Visualizer color rework, examples included

--- jointlab.py
+++ jointlab.py
@@ -5,6 +5,34 @@ import json
 from pyvis.network import Network
 import random
 from datetime import datetime
+import seaborn as sns
+import matplotlib.colors as mcolors
+
+"""
+functions to change/remove when not needed anymore (soon)
+
+reset_graph_list:
+  doesn't need make_complete_graph anymore
+
+load_graphs_json:
+  doesn't need make_complete_graph anymore
+  should call the correct graph_from_row()
+  
+graph_from_row:
+  not needed anymore, superceded by the updated format
+  
+graph_from_row_new:
+  should be renamed, like to complete_graph_from_row
+  update caller refrerences in load_graphs_json
+  
+complete_the_graph:
+  not needed anymore
+  
+make_graph_html:
+  not needed anymore, superceded by the solution only format
+  
+
+"""
 
 def reset_graph_list(json_filepath, make_complete_graph):
   new_graph_list = []
@@ -139,6 +167,50 @@ def make_graph_html(graph_list, canvas_height, solution_only: bool = False, sol_
     net.set_options(' '.join(['var','options','=',json.dumps(vis_opt_dict)]))
     net.save_graph(name='/'.join(['./graphvisuals','.'.join(['_'.join([timestamp, 'graph', str(i)]),'html'])]))
     
+def make_solution_html(graph_list, canvas_height, sol_list: list, vis_opt_dict: dict = {}, color_scale_attr: str = 'weight'):
+  timestamp = datetime.now().strftime("%Y-%m-%d-%H-%M-%S")
+  for i, graph in enumerate(graph_list):
+    print("making solution copy of graph", i)
+    vis_graph: nx.Graph = nx.create_empty_copy(graph)
+    edges = [(sol_list[i][j], sol_list[i][j+1]) for j in range(len(sol_list[i])-1)]
+    for u,v in edges:
+      vis_graph.add_edge(u, v, weight=graph.edges[u,v]['weight'])
+    print("generating graphvis data for graph", i)
+    coords = np.array(list(nx.get_node_attributes(vis_graph, 'pos').values()))
+    x_coords = coords[:, 0]
+    y_coords = coords[:, 1]
+    #scale up the coordinates of drawing to the canvas size (minimul information is pixel, so 1)
+    x_min, x_max = x_coords.min(), x_coords.max()
+    y_min, y_max = y_coords.min(), y_coords.max()
+    x_range = (x_max - x_min) if (x_max != x_min) else 1
+    y_range = (y_max - y_min) if (y_max != y_min) else 1
+    scale = max(x_range, y_range)
+    #These two iterations are where individual attributes can be set for nodes and edges
+    for u, data in vis_graph.nodes(data=True):
+      #apply the scaled x and y values to each coord
+      data['x'] = np.float64(((data['pos'][0] - x_min) / scale) * canvas_height)
+      data['y'] = np.float64(-((data['pos'][1] - y_min) / scale) * canvas_height)
+      data['size'] = 1
+      data['title'] = str(u)
+    (low_scale, high_scale) = get_attribute_extremes(vis_graph, color_scale_attr)
+    normalizer = mcolors.LogNorm(vmin=low_scale, vmax=high_scale)
+    palette = sns.cubehelix_palette(as_cmap=True)
+    for u, v, data in vis_graph.edges(data=True):
+      data['value'] = 1 #data.get('weight', 1)
+      data['width'] = 1 #must add width before converting to pyvis.net or weight attribute gets destroyed
+      data['color'] = get_color_hex_in_range(data[color_scale_attr], palette, normalizer)      #str(#'+hex(random.randrange(0,2**24))[2:])
+      # data['label'] = str(data.get('weight', 1)) #str(data['value'])
+      data['font'] = {"size":1, "strokeWidth":0, "color":"#fffffff"}
+    print("generating graphvis html for graph", i)
+    net = Network(height=canvas_height, width='100%', notebook=False)#, filter_menu=False, select_menu=False)
+    net.from_nx(vis_graph)
+    del vis_graph
+    net.toggle_drag_nodes(False)
+    net.toggle_physics(False)
+    net.show_buttons()#filter_=['nodes', 'edges', 'selection', 'renderer', 'interaction', 'phsyics'])
+    net.set_options(' '.join(['var','options','=',json.dumps(vis_opt_dict)]))
+    net.save_graph(name='/'.join(['./graphvisuals','.'.join(['_'.join([timestamp, 'graph', str(i)]),'html'])]))
+      
 def solve_graphs_greedy(graph_list):
   solution_list = []
   print("Beginning search for solutions")
@@ -152,3 +224,26 @@ def save_solutions(solution_list, solution_filepath):
   print("Saving solution sets")
   sol_array = np.array(solution_list, dtype=np.uint16)
   np.savetxt(solution_filepath,sol_array.transpose(),delimiter=',',fmt='%i')
+
+def add_weights(graph_list):
+  for graph in graph_list:
+    for u, v, data in graph.edges(data=True):
+      if data['alignment'] != 0:
+        data['weight'] = data['alignment']
+      else:
+        data['weight'] = 2 + data['length']
+
+def get_attribute_extremes(graph: nx.Graph, attribute: str):
+  attrList = nx.get_edge_attributes(graph, attribute) #gets iterable list of specified attribute
+  #assume extremes
+  minAtt = float('inf')
+  maxAtt = float('-inf')
+  for edge in attrList:
+    if attrList[edge] > maxAtt:
+      maxAtt = attrList[edge]
+    if attrList[edge] < minAtt:
+      minAtt = attrList[edge]
+  return (minAtt, maxAtt)
+
+def get_color_hex_in_range(value, colormap: mcolors.ListedColormap, normalizer: mcolors.Normalize):
+  return mcolors.to_hex(colormap(normalizer(value)))
